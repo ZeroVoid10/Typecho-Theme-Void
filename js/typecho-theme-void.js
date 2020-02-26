067ef689de1408c9727ce4244444d68f13129bf9
zerovoid <zerovoid10@163.com>
2020-02-26 22:21:50 +0800

    update

--- js/typecho-theme-void.js
+++ js/typecho-theme-void.js
@@ -1,6 +1,5 @@
 // Zero Void 2020-02-24 Mon
 
-// FIXME: 冒泡捕获逻辑来实现
 $("#part").children("li").click(function(){
     $(this).children("a").children("i").toggle();
     if (!$(this).children().hasClass("show") || $(this).children().hasClass("collapsing")) {
